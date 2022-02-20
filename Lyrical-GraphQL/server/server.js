2b22f5ee26b12be03b4aeb62fa225eee4acba829
Brian Bell <brian.bell@dcsg.com>
2022-02-20 09:32:53 -0500

    song create component

--- Lyrical-GraphQL/server/server.js
+++ Lyrical-GraphQL/server/server.js
@@ -8,16 +8,20 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb+srv://dsgpnadsgiunisgd:lxOjsqAhgfpAhLlj@cluster0.m2v6f.mongodb.net/lyricaldb?retryWrites=true&w=majority';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
 
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URI);
+mongoose.connect(MONGO_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
 mongoose.connection
-    .once('open', () => console.log('Connected to MongoLab instance.'))
-    .on('error', error => console.log('Error connecting to MongoLab:', error));
+  .once('open', () => console.log('Connected to MongoLab instance.'))
+  .on('error', error => console.log('Error connecting to MongoLab:', error));
 
 app.use(bodyParser.json());
 app.use('/graphql', expressGraphQL({
