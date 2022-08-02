3fedd6c057214f5e0c2ab35f7090efb60bf859f1
Brian Bell <brian.bell@dcsg.com>
2022-08-02 09:04:44 -0400

    finish requireAuth HOC

--- auth-graphql-starter/client/components/Dashboard.js
+++ auth-graphql-starter/client/components/Dashboard.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Dashboard extends Component {
   render() {
     return (
-      <div>Dashboard</div>
+      <div>You are logged in.</div>
     );
   }
 }
